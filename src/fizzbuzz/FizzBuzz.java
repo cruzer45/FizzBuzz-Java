128106c233cedb918f6a5fe57bff02a65c91df6a
Maurice Rogers <cruzer45@gmail.com>
2013-05-29 11:01:40 -0600

    Initial Commit

---
+++ src/fizzbuzz/FizzBuzz.java
@@ -0,0 +1,34 @@
+package fizzbuzz;
+
+/**
+ *
+ * @author m.rogers
+ */
+public class FizzBuzz
+{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        for (int i = 1; i <= 100; i++)
+        {
+            if ((i % 3 == 0) && (i % 5 == 0))
+            {
+                System.out.println("Fizz Buzz");
+            }
+            else if (i % 3 == 0)
+            {
+                System.out.println("Fizz");
+            }
+            else if (i % 5 == 0)
+            {
+                System.out.println("Buzz");
+            }
+            else{
+                System.out.println(i);
+            }
+        }
+    }
+}
